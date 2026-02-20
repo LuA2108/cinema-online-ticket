f4bd18ae6aa7e130c65730db168505797deafbe5
Lucero <luceroan2108@gmail.com>
2026-02-20 09:39:59 +0100

    Creacion de admin y credenciales de usuarios

--- config/bd_cine_sistema_reserva.sql
+++ config/bd_cine_sistema_reserva.sql
@@ -307,7 +307,8 @@ VALUES
 (2, 'Juan Perez', 'juan@mail.com', 'juan'),
 (2, 'Maria Lopez', 'maria@mail.com', 'maria'),
 (2, 'Carlos Solaz', 'carlos@mail.com', 'carlos'),
-(2, 'Sara Vega', 'sara@mail.com', 'sara');
+(2, 'Sara Vega', 'sara@mail.com', 'sara'),
+(1, 'admin', 'admin@admin.com' 'admin');
 
 -- Insertar reserva
 INSERT INTO reserva (usuario_id, funcion_id, estado_id, total) VALUES
---
+++ config/credenciales.md
@@ -0,0 +1,17 @@
+***📋 CREDENCIALES 📋***
+
+*** Usuerios ***
+- user: juan@gmail.com
+- password: juan
+
+- user: maria@gmial.com
+- password: maria
+
+- user: carlos@gmail.com
+- passwrod: carlos
+
+- user: sara@gmail.com
+- password: sara
+
+- user: admin@admin.com
+- password: admin
