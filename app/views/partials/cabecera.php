640f0914fd8aafdc01532b5f0c2c0a2ea40af5ac
Lucero <luceroan2108@gmail.com>
2026-03-02 13:17:14 +0100

    Cabecera (header) del body, contiene el menu, logo y buscador

---
+++ app/views/partials/cabecera.php
@@ -0,0 +1,50 @@
+<!-- ======= Cabecera : Logo / Buscador / Menu  ======= -->
+    <header>
+        <!-- LOGO -->
+        <div class="logo">
+            <img src="./assets/imgs/iconos/logo_cine.png" alt="" width="100px" height="130px">
+        </div>
+
+        <!-- BUSCADOR -->
+        <div class="buscador">
+            <form action="#" method="get">
+                <img src="./assets/imgs/iconos/buscar.png" alt="Icono buscador" width="64px" height="64px">
+                <input type="search" name="buscador" placeholder="Buscar...">
+            </form>
+        </div>
+
+        <!-- MENU -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container">
+                <a class="navbar-brand" href="#">CineToon</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                Inicio
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Peliculas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cartelera</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Reservar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Sobre Nosotros
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <hr>
+    </header>
