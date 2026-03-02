d94a5a45e49aa2522c1a9cce3d90718dce4b3c36
Lucero <luceroan2108@gmail.com>
2026-03-02 18:15:49 +0100

    Partial - pie de pagina

---
+++ app/views/partials/footer.php
@@ -0,0 +1,81 @@
+<footer>
+    <section>
+        <div>
+            <h2>CineToon</h2>
+            <p>Disfruta de las mejores películas y estrenos exclusivos.</p>
+        </div>
+
+        <div>
+            <h2>Siguenos en redes sociales:</h2>
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fa-brands fa-facebook" style="color: rgb(255, 209, 73);"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa-brands fa-instagram" style="color: rgb(255, 209, 73);"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><img src="#" alt="X (Twiter)">
+                        <i class="fa-brands fa-x-twitter" style="color: rgb(255, 209, 73);"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa-brands fa-youtube" style="color: rgb(255, 209, 73);"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><img src="#" alt="TikTok">
+                        <i class="fa-brands fa-tiktok" style="color: rgb(255, 209, 73);"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section>
+        <h2>Explorar</h2>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Peliculas</a></li>
+            <li><a href="#">Estrenos</a></li>
+            <li><a href="#">Cartelera</a></li>
+            <li><a href="#">Reserva</a></li>
+        </ul>
+    </section>
+    <section>
+        <h2>Cuenta</h2>
+        <ul>
+            <li><a href="#">Iniciar sesion</a></li>
+            <li><a href="#"></a>Registrarse</li>
+            <li><a href="#"></a>Mi perfil</li>
+            <li><a href="#"></a>Mi lista</li>
+        </ul>
+    </section>
+    <section>
+        <h2>Información</h2>
+        <ul>
+            <li><a href="#">Sobre nosotros</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Noticias</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </section>
+    <section>
+        <h2>Legal</h2>
+        <ul>
+            <li><a href="#">Términos y condiciones</a></li>
+            <li><a href="#">Política de privacidad</a></li>
+            <li><a href="#">Política de cookies</a></li>
+        </ul>
+    </section>
+
+    <div>
+        <hr>
+        <p>&copy2026 <span>CineToon</span>. Todos los derechos reservados.</p>
+    </div>
+</footer>
