9367e95dd418d27dab8b0e9c1b5db2c2ef702f7a
Lucero <luceroan2108@gmail.com>
2026-03-02 18:16:08 +0100

    Añadir script de font awesome

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?= $title ?? 'Cinema Online Ticket' ?></title>
+    <title>🎥 <?= $title ?? 'Cinema Online Ticket' ?></title>
     <meta name="description" content="<?= $descripcion ?? 'Cinema Online Ticket'?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="/public/assets/css/<?= $extraCSS ?>.css">
     <?php endif; ?>
 
-    <!-- BootStrap -->
+    <!-- BootStrap y Font Awesome-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/TUCODIGO.js" crossorigin="anonymous"></script>
 </head>
