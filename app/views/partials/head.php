891d7071e13fac3abc0e4d376d3ed295678f4bda
Lucero <luceroan2108@gmail.com>
2026-03-02 13:16:47 +0100

    Partial head para reutilizarlo en varias paginas

---
+++ app/views/partials/head.php
@@ -0,0 +1,19 @@
+<!-- views/partials/head.php -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?= $title ?? 'Cinema Online Ticket' ?></title>
+    <meta name="description" content="<?= $descripcion ?? 'Cinema Online Ticket'?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS global -->
+    <link rel="stylesheet" href="/assets/css/main.css">
+
+    <!-- CSS específico por página -->
+    <?php if (isset($extraCSS)): ?>
+        <link rel="stylesheet" href="/public/assets/css/<?= $extraCSS ?>.css">
+    <?php endif; ?>
+
+    <!-- BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
