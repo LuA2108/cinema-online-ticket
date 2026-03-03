7e993503bf34306dd1b0bd348e6122ea7467803d
Lucero <luceroan2108@gmail.com>
2026-03-03 12:24:43 +0100

    modificacion de rutas y tamaños de las img

--- app/views/partials/cabecera.php
+++ app/views/partials/cabecera.php
@@ -1,44 +1,45 @@
 <!-- ======= Cabecera : Logo / Buscador / Menu  ======= -->
     <header>
+        
         <!-- LOGO -->
         <div class="logo">
-            <img src="./assets/imgs/iconos/logo_cine.png" alt="" width="100px" height="130px">
+            <img src="public/assets/imgs/iconos/logo_cine.png" alt="" width="180px" height="130px">
         </div>
 
         <!-- BUSCADOR -->
         <div class="buscador">
             <form action="#" method="get">
-                <img src="./assets/imgs/iconos/buscar.png" alt="Icono buscador" width="64px" height="64px">
+                <img src="public/assets/imgs/iconos/buscar.png" alt="Icono buscador" width="30px" height="30px">
                 <input type="search" name="buscador" placeholder="Buscar...">
             </form>
         </div>
 
         <!-- MENU -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav class="navegador">
             <div class="container">
                 <a class="navbar-brand" href="#">CineToon</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">
+                <div class="" id="navbarNav">
+                    <ul class="">
+                        <li class="">
+                            <a class="" href="#">
                                 Inicio
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Peliculas</a>
+                        <li class="">
+                            <a class="" href="#">Peliculas</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Cartelera</a>
+                        <li class="">
+                            <a class="" href="#">Cartelera</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Reservar</a>
+                        <li class="">
+                            <a class="" href="#">Reservar</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
+                        <li class="">
+                            <a class="" href="#">
                                 Sobre Nosotros
                             </a>
                         </li>
