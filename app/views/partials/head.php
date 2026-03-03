6136e959ee24d90278bbaa647ac9e58ba670f009
Lucero <luceroan2108@gmail.com>
2026-03-03 17:51:31 +0100

    Eliminacion del enlace a font-awesome

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -8,7 +8,6 @@
 
     <!-- BootStrap y Font Awesome-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/TUCODIGO.js" crossorigin="anonymous"></script>
 
     <!-- CSS global -->
     <link rel="stylesheet" href="public/assets/css/main.css">
