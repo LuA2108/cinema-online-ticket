7e1dc6b1770dff628de93af2f534e37a4be088dc
Lucero <luceroan2108@gmail.com>
2026-03-03 17:51:09 +0100

    Modificacion del menu y adicion del submenu de usuario

--- app/views/partials/cabecera.php
+++ app/views/partials/cabecera.php
@@ -1,65 +1,57 @@
 <!-- ======= Cabecera : Logo / Buscador / Menu  ======= -->
-    <header>
-        
-        <!-- LOGO -->
-        <div class="logo">
-            <img src="public/assets/imgs/iconos/logo_cine.png" alt="" width="180px" height="130px">
-        </div>
+<header>
 
-        <!-- BUSCADOR -->
-        <div class="buscador">
-            <form action="#" method="get">
-                <img src="public/assets/imgs/iconos/buscar.png" alt="Icono buscador" width="30px" height="30px">
-                <input type="search" name="buscador" placeholder="Buscar película...">
-            </form>
-        </div>
+    <!-- LOGO -->
+    <div class="logo">
+        <img src="public/assets/imgs/iconos/logo_cine.png" alt="" width="180px" height="130px">
+    </div>
 
-        <!-- MENU -->
-        <nav class="navegador">
-            <div class="container">
-                <a class="navbar-brand" href="#">CineToon</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="" id="navbarNav">
-                    <ul class="">
-                        <li class="">
-                            <a class="" href="#">
-                                Inicio
-                            </a>
-                        </li>
-                        <li class="">
-                            <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/cartelera.png" alt="Icono cartelera"> Cartelera</a>
-                        </li>
-                        <li class="">
-                            <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/film.png" alt="Icono film"> Peliculas</a>
-                        </li>
-                        <li class="">
-                            <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/boletos-reserva.png" alt="Icono de reserva"> Reservar</a>
-                        </li>
-                        <li class="">
-                            <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/palomitas-de-maiz.png" alt="Icono de palomitas"> Servicios</a>
-                        </li>
-                        <li class="">
-                            <a class="" href="#"> <img src="public/assets/imgs/iconos/informacion.png" alt="Icono de información"> Sobre Nosotros</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <!-- BUSCADOR -->
+    <div class="buscador">
+        <form action="#" method="get">
+            <img src="public/assets/imgs/iconos/buscar.png" alt="Icono buscador" width="30px" height="30px">
+            <input type="search" name="buscador" placeholder="Buscar película...">
+        </form>
+    </div>
 
-        <!-- SUBMENU para SESION-->
-        <nav>
-            <ul>
-                <li>
-                    <a href="#"> <img src="public/assets/imgs/iconos/perfil.png" alt=""></a>
-                    <ul>
-                        <li><a href="">Iniciar sesión</a></li>
-                        <li><a href="">Registrarse</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-        <hr>
-    </header>
+    <!-- MENU -->
+    <nav class="menu">
+        <ul class="">
+            <li class="">
+                <a class="" href="#">
+                    <img class="icono" src="public/assets/imgs/iconos/inicio.png" alt="Icono inicio">
+                    Inicio
+                </a>
+            </li>
+            <li class="">
+                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/cartelera.png" alt="Icono cartelera"> Cartelera</a>
+            </li>
+            <li class="">
+                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/film.png" alt="Icono film"> Peliculas</a>
+            </li>
+            <li class="">
+                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/boletos-reserva.png" alt="Icono de reserva"> Reservar</a>
+            </li>
+            <li class="">
+                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/palomitas-de-maiz.png" alt="Icono de palomitas"> Servicios</a>
+            </li>
+            <li class="">
+                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/informacion.png" alt="Icono de información"> Sobre Nosotros</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- SUBMENU para SESION-->
+    <nav class="menu-sesion">
+        <ul>
+            <li class="dropdown">
+                <a href="#"> <img class="icono-sesion" src="public/assets/imgs/iconos/perfil.png" alt=""></a>
+                <ul class="submenu">
+                    <li><a href="">Iniciar sesión</a></li>
+                    <li><a href="">Registrarse</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <hr>
+</header>
