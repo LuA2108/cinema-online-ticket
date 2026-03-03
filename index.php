cacfa087f89f7168a9989046bd8d6521dc1dbca6
Lucero <luceroan2108@gmail.com>
2026-03-03 10:39:39 +0100

    Añadir el Front controller

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-header("Location: public/");
-exit;
+// Habilitar errores para depuración
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Cargar el autoload
+require __DIR__ . '/app/controllers/MainController.php';
+
+// Determinar qué controlador y acción usar
+$controller = new MainController();
+$controller->index();
