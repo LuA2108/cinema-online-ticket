e6ba6bdb42b29e8ddee968d847cc0a9ce0dc32b5
Lucero <luceroan2108@gmail.com>
2026-03-03 08:56:35 +0100

    Edicion del contenido especifico de index

--- public/index.php
+++ public/index.php
@@ -1,14 +1,32 @@
-<!DOCTYPE html>
-<h1> Proximos estrenos</h1>
+<h1> ¡Vive la magia de <span>CineToon</span>!</h1>
 
 <section id="carrousel-principal">
-    <h2>Nombre pelicula</h2>
+    <h2>Ahora en Pantalla</h2>
+    <section>
+        <h3>Nombre Pelicula</h3>
+        <img src="#" alt="Portada pelicula A">
+        <a href="#" class="btn btn-primary">Comprar Entrada</a>
+    </section>
 </section>
 
 <section id="proximas-peliculas">
-    <h2>Proximas Películas</h2>
+    <h2>Proximos estrenos</h2>
+    <div class="carousel-item">
+        <img src="#" alt="Pelicula estreno A">
+        <h3>Nombre pelicula</h3>
+        <a href="#" class="btn btn-primary">Comprar Entrada</a>
+    </div>
 </section>
 
 <section id="registro-info">
-    <h2>Registrare para ofertas Exclusivas</h2>
+    <h2>¡Mantente al Día con Nuestro Cine!</h2>
+    <p>Regístrate con tu correo electrónico y recibe información sobre estrenos, promociones y eventos especiales.</p>
+
+    <form action="suscripcion.php" method="post" class="newsletter-form d-flex justify-content-center">
+        <input type="email" name="email" placeholder="Tu correo electrónico" required class="form-control me-2" />
+        <button type="submit" class="btn btn-primary">
+            <i class="fa-solid fa-envelope"></i>Suscribirse
+        </button>
+    </form>
+
 </section>
