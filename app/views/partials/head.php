89f70442781f59262c66e87e7d932b0ca20a7ed7
Lucero <luceroan2108@gmail.com>
2026-03-04 19:22:28 +0100

    Agregar el enlace de font awesome

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -8,6 +8,7 @@
 
     <!-- BootStrap y Font Awesome-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/b527a55e68.js" crossorigin="anonymous"></script>
 
     <!-- CSS global -->
     <link rel="stylesheet" href="public/assets/css/main.css">
