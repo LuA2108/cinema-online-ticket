486423e36220280d89cc0288aaea7999248b2f1b
Lucero <luceroan2108@gmail.com>
2026-03-04 19:23:51 +0100

    Cambio de estructura y adicion de clases

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,80 +1,80 @@
 <footer>
-    <section>
-        <div>
+    <div class="container-main">
+        <section class="container-redes-sociales">
             <h2>CineToon</h2>
             <p>Disfruta de las mejores películas y estrenos exclusivos.</p>
-        </div>
 
-        <div>
-            <h2>Siguenos en redes sociales:</h2>
+            <div class="container-icons-redes">
+                <h3>Siguenos en redes sociales:</h3>
+                <ul class="iconos-redes-sociales">
+                    <li>
+                        <a href="#">
+                            <i class="fa-brands fa-facebook icon" style="color: rgb(255, 209, 73);"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa-brands fa-instagram icon" style="color: rgb(255, 209, 73);"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa-brands fa-x-twitter icon" style="color: rgb(255, 209, 73);"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa-brands fa-youtube icon" style="color: rgb(255, 209, 73);"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa-brands fa-tiktok icon" style="color: rgb(255, 209, 73);"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+        <section>
+            <h2>Explorar</h2>
             <ul>
-                <li>
-                    <a href="#">
-                        <i class="fa-brands fa-facebook" style="color: rgb(255, 209, 73);"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa-brands fa-instagram" style="color: rgb(255, 209, 73);"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#"><img src="#" alt="X (Twiter)">
-                        <i class="fa-brands fa-x-twitter" style="color: rgb(255, 209, 73);"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa-brands fa-youtube" style="color: rgb(255, 209, 73);"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#"><img src="#" alt="TikTok">
-                        <i class="fa-brands fa-tiktok" style="color: rgb(255, 209, 73);"></i>
-                    </a>
-                </li>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Peliculas</a></li>
+                <li><a href="#">Estrenos</a></li>
+                <li><a href="#">Cartelera</a></li>
+                <li><a href="#">Reserva</a></li>
             </ul>
-        </div>
-    </section>
+        </section>
+        <section>
+            <h2>Cuenta</h2>
+            <ul>
+                <li><a href="#">Iniciar sesion</a></li>
+                <li><a href="#">Registrarse</a></li>
+                <li><a href="#">Mi perfil</a></li>
+                <li><a href="#">Mi lista</a></li>
+            </ul>
+        </section>
+        <section>
+            <h2>Información</h2>
+            <ul>
+                <li><a href="#">Sobre nosotros</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Noticias</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </section>
+        <section>
+            <h2>Legal</h2>
+            <ul>
+                <li><a href="#">Términos y condiciones</a></li>
+                <li><a href="#">Política de privacidad</a></li>
+                <li><a href="#">Política de cookies</a></li>
+            </ul>
+        </section>
+    </div>
 
-    <section>
-        <h2>Explorar</h2>
-        <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Peliculas</a></li>
-            <li><a href="#">Estrenos</a></li>
-            <li><a href="#">Cartelera</a></li>
-            <li><a href="#">Reserva</a></li>
-        </ul>
-    </section>
-    <section>
-        <h2>Cuenta</h2>
-        <ul>
-            <li><a href="#">Iniciar sesion</a></li>
-            <li><a href="#">Registrarse</a></li>
-            <li><a href="#">Mi perfil</a></li>
-            <li><a href="#">Mi lista</a></li>
-        </ul>
-    </section>
-    <section>
-        <h2>Información</h2>
-        <ul>
-            <li><a href="#">Sobre nosotros</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Noticias</a></li>
-            <li><a href="#">Contacto</a></li>
-        </ul>
-    </section>
-    <section>
-        <h2>Legal</h2>
-        <ul>
-            <li><a href="#">Términos y condiciones</a></li>
-            <li><a href="#">Política de privacidad</a></li>
-            <li><a href="#">Política de cookies</a></li>
-        </ul>
-    </section>
-
-    <div>
+    <div class="copy">
         <hr>
         <p>&copy; 2026 <span>CineToon</span>. Todos los derechos reservados.</p>
     </div>
