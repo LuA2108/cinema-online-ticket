2800eb23312d665f76422fc7b697cf7f75198c5f
Lucero <luceroan2108@gmail.com>
2026-03-08 21:10:23 +0100

    Adicion de estructura de carousel al indice de la web

--- app/views/public/index.php
+++ app/views/public/index.php
@@ -2,11 +2,48 @@
 
 <section id="carrousel-principal">
     <h2>Ahora en Pantalla</h2>
-    <section>
-        <h3>Nombre Pelicula</h3>
-        <img src="#" alt="Portada pelicula A">
-        <a href="#" class="btn btn-primary">Comprar Entrada</a>
-    </section>
+    
+    <div class="container mt-5">
+        <div id="carousel-peliculas" class="carousel slide" data-bs-ride="carousel">
+
+            <!-- Indicadores -->
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carousel-peliculas" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#carousel-peliculas" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#carousel-peliculas" data-bs-slide-to="2"></button>
+            </div>
+
+            <!-- CarruselImg -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <h3 class="titulo-pelicula">Inception</h3>
+                    <img src="public/assets/imgs/films/banner/inception_hor.jpg" class="d-block w-100" alt="Portada Inception">
+                    <a href="#" type="button">Reservar</a>
+                </div>
+                <div class="carousel-item">
+                    <h3 class="titulo-pelicula">Avatar</h3>
+                    <img src="public/assets/imgs/films/banner/avatarBanner.jpg" class="d-block w-100" alt="Portada Avatar">
+                    <a href="#" type="button">Reservar</a>
+                </div>
+                <div class="carousel-item">
+                    <h3 class="titulo-pelicula">Jocker</h3>
+                    <img src="public/assets/imgs/films/banner/jocker_hor.png" class="d-block w-100 h-50" alt="Portada Jocker">
+                    <a href="#" type="button">Reservar</a>
+                </div>
+            </div>
+
+            <!-- Controladores - Anterior & Siguiente -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#carousel-peliculas" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+                <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel-peliculas" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+                <span class="visually-hidden">Siguiente</span>
+            </button>
+        </div>
+
+    </div>
 </section>
 
 <section id="proximas-peliculas">
