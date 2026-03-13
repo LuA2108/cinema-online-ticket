2d456b96d029a4ee25b5be1c56aebae6e41863df
Lucero <luceroan2108@gmail.com>
2026-03-13 17:38:19 +0100

    Cambio de estructura de la cabecera y adicion de clases a las listas

--- app/views/partials/cabecera.php
+++ app/views/partials/cabecera.php
@@ -1,57 +1,53 @@
 <!-- ======= Cabecera : Logo / Buscador / Menu  ======= -->
 <header>
 
-    <!-- LOGO -->
-    <div class="logo">
-        <img src="public/assets/imgs/iconos/logo_cine.png" alt="Icono logo cine" width="150px" height="100px">
+    <div class="contenedor-logo-sesion">
+        <!-- LOGO -->
+        <div class="logo">
+            <a href="index.php"><img src="public/assets/imgs/iconos/logo_cine.png" alt="Icono logo cine" width="170px" height="100px"></a>
+        </div>
+
+        <!-- SUBMENU para SESION-->
+        <nav class="menu-sesion">
+            <ul>
+                <li class="dropdown">
+                    <a href="#"> <i class="fa-solid fa-user" style="color: rgb(239, 244, 255);"></i></a>
+                    <ul class="submenu">
+                        <li><a href="">Iniciar sesión</a></li>
+                        <li><a href="">Registrarse</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
     </div>
 
-    <!-- BUSCADOR -->
-    <div class="buscador">
-        <form action="#" method="get">
-            <img src="public/assets/imgs/iconos/buscar.png" alt="Icono buscador" width="30px" height="30px">
-            <input type="search" name="buscador" placeholder="Buscar película...">
-        </form>
-    </div>
-
-    <!-- MENU -->
-    <nav class="menu">
-        <ul class="">
-            <li class="">
-                <a class="" href="#">
-                    <img class="icono" src="public/assets/imgs/iconos/inicio.png" alt="Icono inicio">
-                    Inicio
-                </a>
-            </li>
-            <li class="">
-                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/cartelera.png" alt="Icono cartelera"> Cartelera</a>
-            </li>
-            <li class="">
-                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/film.png" alt="Icono film"> Peliculas</a>
-            </li>
-            <li class="">
-                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/boletos-reserva.png" alt="Icono de reserva"> Reservar</a>
-            </li>
-            <li class="">
-                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/palomitas-de-maiz.png" alt="Icono de palomitas"> Servicios</a>
-            </li>
-            <li class="">
-                <a class="" href="#"> <img class="icono" src="public/assets/imgs/iconos/informacion.png" alt="Icono de información"> Sobre Nosotros</a>
-            </li>
-        </ul>
-    </nav>
-
-    <!-- SUBMENU para SESION-->
-    <nav class="menu-sesion">
-        <ul>
-            <li class="dropdown">
-                <a href="#"> <img class="icono-sesion" src="public/assets/imgs/iconos/perfil.png" alt=""></a>
-                <ul class="submenu">
-                    <li><a href="">Iniciar sesión</a></li>
-                    <li><a href="">Registrarse</a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
+    <!-- Menu y buscador -->
+    <div class="container-menu-buscador">
+        <!-- MENU -->
+        <nav class="menu">
+            <ul class="">
+                <li class="menu-item">
+                    <a class="item-peliculas" href="#"> <i class="fa-solid fa-clapperboard"></i> Películas</a>
+                </li>
+                <li class="menu-item">
+                    <a class="item-promociones" href="#"><i class="fa-solid fa-euro-sign"></i> Promociones</a>
+                </li>
+                <li class="menu-item">
+                    <a class="item-servicios" href="#"> <img class="icono" src="public/assets/imgs/iconos/palomitas-de-maiz.png" alt="Icono de palomitas"> Servicios</a>
+                </li>
+                <li class="menu-item">
+                    <a class="item-info" href="#"> <i class="fa-solid fa-info"></i>Sobre Nosotros</a>
+                </li>
+                <li class="menu-item">
+                    <a class="item-reserva" href="#"> <i class="fa-solid fa-ticket"></i> Reservar película</a>
+                </li>
+                <li class="menu-item buscador">
+                    <img src="public/assets/imgs/iconos/buscar.png" alt="Icono buscador" width="30" height="30">
+                    <form action="#" method="get">
+                        <input type="search" name="buscador" placeholder="Buscar película...">
+                    </form>
+                </li>
+            </ul>
+        </nav>
 </header>
 <hr>
