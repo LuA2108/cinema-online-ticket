f300899b37e84ea2f048bbd642a4af5ec40341eb
Lucero <luceroan2108@gmail.com>
2026-03-16 14:20:28 +0100

    Creacion layout para la pagina de admin

---
+++ app/views/layout/admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<!-- HEAD -->
+<?php require __DIR__ . "/../partials/head.php"; ?>
+
+<body class="admin">
+    <!-- CABECERA -->
+    <?php require __DIR__ . "/../partials/sidebar.php"; ?>
+    
+    <main class="main-admin">
+        <?= $content; ?> <!-- Aquí se inserta la vista que prepara el controlador -->
+    </main>
+
+    <script src="" async defer></script>
+</body>
+
+</html>
