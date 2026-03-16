9f2abebe0f8bf090b3e208634dd5f22312aa6775
Lucero <luceroan2108@gmail.com>
2026-03-16 13:54:36 +0100

    Creacion del elemento aside para el administrador

---
+++ app/views/partials/sidebar.php
@@ -0,0 +1,31 @@
+<div class="header-admin"></div>
+<input type="checkbox" id="openSidebarMenu">
+<label for="openSidebarMenu" class="sidebarIconToggle">
+    <div class="spinner diagonal part-1"></div>
+    <div class="spinner horizontal"></div>
+    <div class="spinner diagonal part-2"></div>
+</label>
+
+<aside class="sidebar-menu">
+    <div class="container-admin">
+        <img src="#" alt="Imagen de Administrador">
+        <h2>Administrador</h2>
+    </div>
+    <hr>
+    <ul class="sidebar-menu-inner">
+        <li>Mi Web <span>Panel</span></li>
+        <li><a href="#"><i class="fa-solid fa-house" style="color: rgb(239, 244, 255);"></i>Inicio</a></li>
+        <li><a href="#"><i class="fa-solid fa-user" style="color: rgb(239, 244, 255);"></i>Usuarios</a></li>
+        <li><a href="#"><i class="fa-solid fa-video" style="color: rgb(239, 244, 255);"></i>Películas</a></li>
+        <li><a href="#"><i class="fa-solid fa-masks-theater" style="color: rgb(239, 244, 255);"></i>Géneros películas</a></li>
+        <li><a href="#"><i class="fa-solid fa-tv" style="color: rgb(239, 244, 255);"></i>Funciones</a></li>
+        <li><a href="#"><i class="fa-solid fa-calendar" style="color: rgb(239, 244, 255);"></i>Reservas</a></li>
+        <li><a href="#"><i class="fa-solid fa-clapperboard" style="color: rgb(239, 244, 255);"></i>Salas</a></li>
+        <li><a href="#"><i class="fa-solid fa-candy-cane" style="color: rgb(239, 244, 255);"></i>Productos</a></li>
+    </ul>
+
+    <form action="#" method="POST">
+        <button type="submit">Cerrar sesión</button>
+    </form>
+</aside>
+
