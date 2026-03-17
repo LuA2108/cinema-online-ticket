99eac97613e23df6d0c441d40cf7532bfe7caeb3
Lucero <luceroan2108@gmail.com>
2026-03-17 21:31:44 +0100

    Creacion de atributos del modelo de usuario y sus getters & setters

---
+++ app/models/usuario.php
@@ -0,0 +1,103 @@
+<?php
+class Usuario
+{
+    private $id;
+    private $id_rol;
+    private $nombre;
+    private $email;
+    private $contrasena;
+    private $ciudad;
+    private $provincia;
+    private $fechaRegistro;
+
+
+    
+    // Getters y setters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getIdRol()
+    {
+        return $this->id_rol;
+    }
+
+    public function setIdRol($id_rol)
+    {
+        $this->id_rol = $id_rol;
+        return $this;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getContrasena()
+    {
+        return $this->contrasena;
+    }
+
+    public function setContrasena($contrasena)
+    {
+        $this->contrasena = $contrasena;
+        return $this;
+    }
+
+    public function getCiudad()
+    {
+        return $this->ciudad;
+    }
+
+    public function setCiudad($ciudad)
+    {
+        $this->ciudad = $ciudad;
+        return $this;
+    }
+
+    public function getProvincia()
+    {
+        return $this->provincia;
+    }
+
+    public function setProvincia($provincia)
+    {
+        $this->provincia = $provincia;
+        return $this;
+    }
+
+    public function getFechaRegistro()
+    {
+        return $this->fechaRegistro;
+    }
+
+    public function setFechaRegistro($fechaRegistro)
+    {
+        $this->fechaRegistro = $fechaRegistro;
+        return $this;
+    }
+}
