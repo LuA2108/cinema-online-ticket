c44b06ea3c00c747b4a5682923dc8958d98258ad
Lucero <luceroan2108@gmail.com>
2026-04-27 15:37:03 +0200

    Edicion de usuario y adicion de la funcion buscar usuario por email

--- app/models/usuario.php
+++ app/models/usuario.php
@@ -10,15 +10,6 @@ require_once 'config/database.php';
 class Usuario
 {   
     private $conn;
-    private $id;
-    private $id_rol;
-    private $nombre;
-    private $email;
-    private $contrasena;
-    private $ciudad;
-    private $provincia;
-    private $fechaRegistro;
-
 
     /**
      * Constructor de la clase
@@ -44,92 +35,17 @@ class Usuario
         return $usuarios;
     }
     
-    // Getters y setters de las propiedades de la clase
-    public function getId()
-    {
-        return $this->id;
+    /**
+     * Función que devuelve un usuario segun el email
+     * @param $email correo del usuario
+     * @return array|bool|null array con datos del usuario   
+     */
+    public function buscarPorEmail($email) {
+        $stmt = $this->conn->prepare("SELECT * FROM usuarios WHERE email = ? ");
+        $stmt -> bind_param("s", $email);
+        $stmt->execute();
+
+        return $stmt->get_result()->fetch_assoc();
     }
 
-    public function setId($id)
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    public function getIdRol()
-    {
-        return $this->id_rol;
-    }
-
-    public function setIdRol($id_rol)
-    {
-        $this->id_rol = $id_rol;
-        return $this;
-    }
-
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
-
-    public function setNombre($nombre)
-    {
-        $this->nombre = $nombre;
-        return $this;
-    }
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function setEmail($email)
-    {
-        $this->email = $email;
-        return $this;
-    }
-
-    public function getContrasena()
-    {
-        return $this->contrasena;
-    }
-
-    public function setContrasena($contrasena)
-    {
-        $this->contrasena = $contrasena;
-        return $this;
-    }
-
-    public function getCiudad()
-    {
-        return $this->ciudad;
-    }
-
-    public function setCiudad($ciudad)
-    {
-        $this->ciudad = $ciudad;
-        return $this;
-    }
-
-    public function getProvincia()
-    {
-        return $this->provincia;
-    }
-
-    public function setProvincia($provincia)
-    {
-        $this->provincia = $provincia;
-        return $this;
-    }
-
-    public function getFechaRegistro()
-    {
-        return $this->fechaRegistro;
-    }
-
-    public function setFechaRegistro($fechaRegistro)
-    {
-        $this->fechaRegistro = $fechaRegistro;
-        return $this;
-    }
 }
