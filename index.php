39698307a0dc8c40acfb0c8932cd8bbaceb346c8
Lucero <luceroan2108@gmail.com>
2026-04-27 15:51:27 +0200

    Edicion de la estructura mvc e integracion de sesion y bd

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <?php
+// Inicia sesion 
+session_start();
+
 // Habilitar errores para depuración
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Cargar el controlador
-require __DIR__ . '/app/controllers/pageController.php';
+// Cargar base datos
+require __DIR__ . '/config/database.php';
 
+//Cargar controladores
+require __DIR__ . '/app/controllers/pageController.php';
+require __DIR__ . '/app/controllers/usuarioController.php';
+
+//Cargar Modelos 
+require_once __DIR__ . '/app/models/usuario.php';
+
+
+// Dependencias, conexion a la BD 
+$db = new Database();
+$conn = $db->obtenerConexion();
+
+//Obtener pagina (por defecto index)
 $page = $_GET['page'] ?? 'index';
 
-// Crear instancia del controlador
-$controller = new PageController();
+// Instancia del modelo usuario
+$usuarios = new Usuario($conn);
 
-// llamar al método principal
-$controller->mostrarPaginas($page);
+// Instancia del controlador 
+$controlador = new pageController($conn);
+
+// El controlador gestiona las paginas
+$controlador->mostrarPagina($page);
+?>
