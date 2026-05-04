088ee05d2201524cb24778dd12301cebe5f8a61d
Lucero <luceroan2108@gmail.com>
2026-05-04 12:16:10 +0200

    Edicion de la vista del login para el menajo de error

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,24 +1,29 @@
-<?php 
-
-?>
 <section class="container_login">
     <h1>Bienvenido</h1>
 
     <div class="auth_form">
         <h2 class="login_titulo">Iniciar sesión</h2>
-        
+
         <!-- Formulario - inicio sesión -->
-        <form action="index.php?page=auth/login.php" method="post">
-            
+        <form action="index.php?page=auth/login" method="post">
+
             <!-- Email  -->
             <label for="email">Email</label>
-            <input type="text" id="email" name="email"  placeholder="Correo">
-            
+            <input type="text" id="email" name="email" placeholder="Correo">
+
             <!-- Contraseña -->
             <label for="password">Contraseña</label>
-            <input type="password" id="password" name="password" placeholder="Contraseña">
-            
-            
+            <input type="password" id="password" name="contrasena" placeholder="Contraseña">
+
+            <?php if (!empty($_SESSION['error'])): ?>
+                <div class="error_login">
+                    <p>
+                        <?php echo $_SESSION['error']; ?>
+                    </p>
+                </div>
+            <?php unset($_SESSION['error']); ?>
+            <?php endif; ?>
+
             <!-- Confirmacion y enlace -->
             <button class="button" type="submit">Enviar</button>
             <p>¿No tienes cuenta? <a href="index.php?page=auth/registro">Registrate</a></p>
