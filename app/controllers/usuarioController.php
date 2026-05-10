37242ab962e12ad4bc1650908057a8db878b3b67
Lucero <luceroan2108@gmail.com>
2026-05-10 21:21:28 +0200

    Eliminacion del login y logout al creacion un controlador para autenticación.

--- app/controllers/usuarioController.php
+++ app/controllers/usuarioController.php
@@ -8,18 +8,17 @@ require_once __DIR__ . "/../models/usuario.php";
  */
 class UserControlador
 {
-
     private $usuario;
 
     /**
-     * Constructor del controlador de usuarios
-     * @param $usuario Objeto del modelo usuario 
+     * Contructor del controlador de usuarios
+     * @param mixed $conn
      */
-    public function __construct(Usuario $usuario)
+    public function __construct($conn)
     {
-        $this->usuario = $usuario;
+        $this->usuario = new Usuario($conn);
     }
-
+    
     /**
      * Obtiene la lista de todos los usuarios
      * @return array Lista de usuarios
@@ -27,55 +26,5 @@ class UserControlador
     public function listarUsuarios()
     {
         return $this->usuario->obtenerUsuarios();
-    }
-
-    /**
-     * Gestiona el inicio de sesion del usuario.
-     * 
-     * Verifica si el usuario existe y si la contraseña es correcta. En caso
-     * valido redirige segun el rol 
-     * @param string $email correo electronico del usuario
-     * @param string $contrasena contraseña introducida 
-     * @return bool devuelve True si es correcto
-     */
-    public function login($email, $contrasena)
-    {
-        $usuario = $this->usuario->buscarPorEmail($email);
-
-        // Comprobar existencia de usuario
-        if (!$usuario) {
-            return false;
-        }
-
-        // Conprobar contraseña
-        if (!password_verify($contrasena, $usuario['contrasena'])) {
-            return false;
-        }
-
-        // Creacion de sesion al ser correcto el login
-        $_SESSION['user_id'] = $usuario['id'];
-        $_SESSION['rol_id'] = $usuario['rol_id'];
-        $_SESSION['email'] = $usuario['email'];
-
-        // Redirección al panel de Admin 
-        if ($usuario['rol_id'] == 1) {
-            header("Location: index.php?page=admin/index");
-            exit;
-        }
-
-        header("Location: index.php?page=index");
-        exit;
-
-        return true;
-    }
-
-
-    public function logout()
-    {
-        session_unset();
-        session_destroy();
-
-        header("Location: index.php?page=index");
-        exit;
     }
 }
