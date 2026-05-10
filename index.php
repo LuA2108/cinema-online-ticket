dc47d3ae1a51b01bbf08c9040e66f7ba6c4c1f37
Lucero <luceroan2108@gmail.com>
2026-05-10 21:26:05 +0200

    Edición del index para centralizar la logica en 'pageController' y simplificar 'index.php'

--- index.php
+++ index.php
@@ -9,13 +9,8 @@ error_reporting(E_ALL);
 // Cargar base datos
 require __DIR__ . '/config/database.php';
 
-//Cargar controladores
+//Cargar controlador principal
 require __DIR__ . '/app/controllers/pageController.php';
-require __DIR__ . '/app/controllers/usuarioController.php';
-
-//Cargar Modelos 
-require_once __DIR__ . '/app/models/usuario.php';
-
 
 // Dependencias, conexion a la BD 
 $db = new Database();
@@ -24,28 +19,10 @@ $conn = $db->obtenerConexion();
 //Obtener pagina (por defecto index)
 $page = $_GET['page'] ?? 'index';
 
-// Instancia del modelo usuario
-$usuario = new Usuario($conn);
-$userController = new UserControlador($usuario);
-
-// Instancia del controlador 
+// Controlador principal
 $controlador = new pageController($conn);
 
-if($page === 'auth/login' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+// Ejecutar
+$controlador->cargarPaginas($page);
 
-    $email = $_POST['email'] ?? '';
-    $contrasena = $_POST['contrasena'] ?? '';
-
-    //Si falla, asigna mensaje de error
-    if(!$userController->login($email, $contrasena)) {
-        $_SESSION['error'] = "Usuario o contraseña incorrecto.";
-    }
-}
-
-if($page === 'auth/logout') {
-    $userController->logout();
-}
-
-// El controlador gestiona las paginas
-$controlador->mostrarPagina($page);
 ?>
