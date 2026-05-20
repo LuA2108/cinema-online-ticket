289366234ae22c06e80b4ac8677bc1e4bf9fa63f
Lucero <luceroan2108@gmail.com>
2026-05-20 16:03:35 +0200

    -Edición de comentarios en la clase y funciones. -Creación de funciones para actualizar y eliminar usuarios en la base de datos.

--- backend/src/models/usuario.php
+++ backend/src/models/usuario.php
@@ -2,9 +2,9 @@
 require_once 'config/database.php';
 
 /**
- * Clase que gestiona a los usuarios:
- * crear, editar, eliminar y consultar datos
- * Utilizanado una conexion a la BD para ejecutar las consultas sql
+ * Clase Usuario
+ * Gestiona operaciones CRUD sobre la tabla usuario
+ * Usa una conexion mysqli mediante inyección de dependencias
  * @author lucero Anay Cahuana
  */
 class Usuario
@@ -21,8 +21,8 @@ class Usuario
     }
 
     /**
-     * Obtiene todos de usuarios de la BD
-     * @return array Lista de usuarios
+     * Obtiene todos los usuarios de la base de datos
+     * @return array Array Lista de usuarios registrados
      */
     public function obtenerUsuarios() {
         $sql = "SELECT * FROM usuario";
@@ -36,9 +36,9 @@ class Usuario
     }
     
     /**
-     * Función que devuelve un usuario segun el email
+     * Obtiene los datos del usuario según su email
      * @param string $email correo del usuario
-     * @return array|bool|null array con datos del usuario   
+     * @return array Array con datos del usuario   
      */
     public function buscarPorEmail($email) {
         $stmt = $this->conn->prepare("SELECT * FROM usuario WHERE email = ? ");
@@ -48,27 +48,49 @@ class Usuario
         return $stmt->get_result()->fetch_assoc();
     }
     /**
-     * Función que inserta un usuario a la Base de datos.
+     * Crea un nuevo usuario en la Base de datos.
      * @param int $rol_id rol de usuario ()
      * @param string $nombre
      * @param string $email
      * @param string $contrasena
      * @param string $ciudad
      * @param string $provincia
-     * @return bool
+     * @return bool True si se creo correctamente, False si fallo
      */
     public function crearUsuario($rol_id, $nombre, $email, $contrasena, $ciudad, $provincia)
     {
-        $sql = $this->conn->prepare("INSERT INTO usuario(rol_id, nombre, email, contrasena, ciudad, privincia) VALUES (?,?,?,?,?,?)");
+        $sql = $this->conn->prepare("INSERT INTO usuario(rol_id, nombre, email, contrasena, ciudad, provincia) VALUES (?,?,?,?,?,?)");
         $sql->bind_param("isssss", $rol_id, $nombre, $email, $contrasena, $ciudad, $provincia);
 
         return $sql->execute();
     }
 
-    public function actualizarUsuario() {
+    /**
+     * Actualiza los datos de un usuario existente
+     * @param int $rol_id
+     * @param string $nombre
+     * @param string $email
+     * @param string $contrasena
+     * @param string $ciudad
+     * @param string $provincia
+     * @param int $id_usuario
+     * @return bool True si se actualizo, False en caso contrario
+     */
+    public function actualizarUsuario($rol_id, $nombre, $email, $contrasena, $ciudad, $provincia, $id_usuario) {
+        $sql = $this->conn->prepare("UPDATE usuario SET rol_id = ?, email= ?, nombre = ?, contrasena = ?, ciudad = ?, provincia = ? WHERE id = ?");
+        $sql->bind_param("isssssi", $rol_id, $email, $nombre, $contrasena, $ciudad, $provincia, $id_usuario);    
 
+        return $sql->execute();
     }
-    public function eliminarUsuario() {
 
+    /*
+     * limina un usuario de la base de datos según su ID
+     * @param int $usuario_id ID del usuario
+     * @return bool True si se elimino, false al fallar
+     */
+    public function eliminarUsuario($usuario_id) {
+        $sql = $this->conn->prepare("DELETE FROM usuario WHERE id = ?");
+        $sql ->bind_param("i", $usuario_id);
+        return $sql->execute();
     }
 }
