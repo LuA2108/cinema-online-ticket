c39841b7c318e3aa1e8a5f3c84a5e2280d905dc5
Lucero <luceroan2108@gmail.com>
2026-05-24 16:12:12 +0200

    Modificación para que las funciones devuelvan datos y no json

--- backend/src/controllers/peliculaController.php
+++ backend/src/controllers/peliculaController.php
@@ -10,7 +10,6 @@ class PeliculaController
 
     /**
      * Constructor de la clase controlador película
-     * @param mysqli $conn Conexion a la BD
      */
     public function __construct($conn)
     {
@@ -19,30 +18,27 @@ class PeliculaController
 
     /**
      * Obtiene una lista de todas las películas de la base de datos y la devuelve en formato JSON
-     * @return void
      */
     public function index()
     {
-        echo json_encode($this->peliculaService->listarPeliculas());
+        return $this->peliculaService->listarPeliculas();
     }
 
     public function listarPeliculasCompletas()
     {
-        echo json_encode($this->peliculaService->listarPeliculasCompletas());
+        return $this->peliculaService->listarPeliculasCompletas();
     }
 
     /**
      * Obtiene una película por su ID y la devuelve en formato JSON
      * @param int $id
-     * @return void
      */
     public function mostrarPelicula($id)
     {
-        echo json_encode($this->peliculaService->obtenerPeliculaPorId($id));
+        return $this->peliculaService->obtenerPeliculaPorId($id);
     }
     /**
      * Guarda una nueva película en la base de datos utilizando los datos proporcionados en el cuerpo de la solicitud HTTP
-     * @return void
      */
     public function guardarPelicula()
     {
@@ -51,7 +47,7 @@ class PeliculaController
         $generos = $datos['generos'];
 
         $resultado = $this->peliculaService->crearPelicula($pelicula, $generos);
-        echo json_encode($resultado);
+        return $resultado;
     }
 
     public function actualizarPelicula()
@@ -61,25 +57,23 @@ class PeliculaController
         $generos = $datos['generos'];
 
         $resultado = $this->peliculaService->actualizarPelicula($pelicula['id'], $pelicula, $generos);
-        echo json_encode($resultado);
+        return $resultado;
     }
     /**
      * Desactiva una película de la base de datos utilizando su ID
      * @param int $id
-     * @return void
      */
     public function activar($id)
     {
-        echo json_encode($this->peliculaService->activarPelicula($id));
+        return $this->peliculaService->activarPelicula($id);
     }
 
     /**
      * Summary of desactivar
      * @param mixed $id
-     * @return void
      */
     public function desactivar($id)
     {
-        echo json_encode($this->peliculaService->desactivarPelicula($id));
+        return $this->peliculaService->desactivarPelicula($id);
     }
 }
