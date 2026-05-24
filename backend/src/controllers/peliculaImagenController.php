62bd4d8da2be5f9c53f785aebacc184e2fb64ed8
Lucero <luceroan2108@gmail.com>
2026-05-24 20:24:08 +0200

    Añadir PeliculaImagenController con inyección de dependencias

---
+++ backend/src/controllers/peliculaImagenController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Service\PeliculaImagenService;
+
+/**
+ * Clase PeliculaImagenController
+ * Controlador para manejar las solicitudes relacionadas con las imágenes de películas, utilizando el servicio PeliculaImagenService para la lógica de negocio
+ * Inyección de dependencias para el servicio necesario en el controlador
+ */
+class PeliculaImagenController
+{
+
+    private PeliculaImagenService $peliculaImagenService;
+
+    /**
+     * Constructor de la clase PeliculaImagenController
+     * @param PeliculaImagenService $peliculaImagenService Servicio para manejar la lógica de negocio relacionada con las imágenes de películas
+     * Inyección de dependencias para el servicio necesario en el controlador
+     */
+    public function __construct($peliculaImagenService)
+    {
+        $this->peliculaImagenService = $peliculaImagenService;
+    }
+
+    public function index()
+    {
+        return $this->peliculaImagenService->listarTodas();
+    }
+
+    public function imagenesPorPelicula(int $peliculaId)
+    {
+        return $this->peliculaImagenService->listarImagenesPorPelicula($peliculaId);
+    }
+
+    public function obtenerImagen(int $imagenId)
+    {
+        return $this->peliculaImagenService->obtenerImagen($imagenId);
+    }
+
+    /**
+     * Summary of crearImagen
+     * @param array $datos
+     * @throws \InvalidArgumentException
+     */
+    public function crearImagen(array $datos)
+    {
+        return $this->peliculaImagenService->agregar(
+            (int)$datos['pelicula_id'],
+            $datos['tipo'],
+            $datos['url']
+        );
+    }
+
+    public function eliminarImagen(int $imagenId)
+    {
+        return $this->peliculaImagenService->eliminar($imagenId);
+    }
+}
