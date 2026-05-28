34f9f177a0148238e4e7219a6c076c581bc0376b
Lucero <luceroan2108@gmail.com>
2026-05-28 19:33:45 +0200

    validar acceso y agregar cierre de sesión a usuario

---
+++ public/vite-project/src/auth/perfil.js
@@ -0,0 +1,22 @@
+const token = localStorage.getItem("token");
+
+if (!token) {
+    window.location.href = "../../index.html";
+}
+
+fetch("http://localhost/cinema-online-ticket/api/perfil", {
+    headers: {
+        Authorization: "Bearer " + token
+    }
+})
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+});
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    localStorage.removeItem("token");
+
+    // redirigir a login
+    window.location.href = "../../index.html";
+});
