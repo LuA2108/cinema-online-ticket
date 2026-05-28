fd5b2b4d4268a41d3f9f838bd6a1a848588d9da3
Lucero <luceroan2108@gmail.com>
2026-05-28 19:18:13 +0200

    - Se agregó el funcionamiento del formulario de login. - Ahora el usuario puede iniciar sesión desde la página, se guarda el token recibido y redirige automáticamente según el tipo de rol: administrador o usuario.

---
+++ public/vite-project/src/auth/login.js
@@ -0,0 +1,29 @@
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const contrasena = e.target.contrasena.value;
+
+    const res = await fetch("http://localhost/cinema-online-ticket/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, contrasena })
+    });
+
+    const data = await res.json();
+
+    if (!data.token) {
+        alert("Login incorrecto");
+        return;
+    }
+    
+    localStorage.setItem("token", data.token);
+
+    const payload = JSON.parse(atob(data.token.split(".")[1]));
+
+    if (payload.rol === 1) {
+        location.href = "../../admin.html";
+    } else {
+        location.href = "../../user/perfil.html";
+    }
+});
