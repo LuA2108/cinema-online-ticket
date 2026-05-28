44a01e1087768a04184493cbb224d7cffb42012c
Lucero <luceroan2108@gmail.com>
2026-05-28 19:16:53 +0200

    Agregar validación de acceso con token: - Se agregó una capa de seguridad para comprobar que el usuario tenga un token válido antes de acceder a ciertas partes del sistema.
    - También se agregó una validación especial para permitir el acceso solo a usuarios administradores cuando sea necesario.

---
+++ backend/src/middleware/authMiddleWare.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\authMiddleware;
+
+use Exception;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class AuthMiddleWare
+{
+    public static function verificarToken()
+    {
+        // Obtener y leer Authorization Header
+        $headers = getallheaders();
+        $authHeader = $headers['Authorization'] ?? null;
+
+        // 1. comprobar que existe
+        if (!$authHeader) {
+            http_response_code(401);
+            exit("Token requerido");
+        }
+
+        // Extraer token (quitar Bearer)
+        $token = str_replace("Bearer ", "", $authHeader);
+
+        try {
+            // 2. comprobar firma + expiración
+            return JWT::decode(
+                $token,
+                new Key("MI_CLAVE_SECRETA_LEO_DANMEI_QUIERO_UN_GATO_LLAMADO_NEFERPITOU_DE_COLOR_NEGRO", "HS256")
+            );
+        } catch (Exception $e) {
+            http_response_code(401);
+
+            echo json_encode([
+                "error" => "Token inválido"
+            ]);
+            exit;
+        }
+    }
+
+    public static function checkAdmin()
+    {
+        $user = self::verificarToken();
+
+        if ($user->rol !== 1) {
+            http_response_code(403);
+            exit("Solo admin");
+        }
+
+        return $user;
+    }
+}
