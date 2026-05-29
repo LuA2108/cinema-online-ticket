8b97dcf8a4d82eac19cd5a719866a40c98685ef9
Lucero <luceroan2108@gmail.com>
2026-05-29 17:51:05 +0200

    Añadir modelo genero para importar los metodos de la api, e incluir los getters/setters y constructor

--- public/vite-project/src/models/modeloGenero.js
+++ public/vite-project/src/models/modeloGenero.js
@@ -1,14 +1,44 @@
-import {obtenerGeneros} from "../api/generosApi";
-
+import { obtenerGeneros, agregarGenero, eliminarGenero, obtenerGenero } from "../api/generosApi.js";
 class ModeloGenero {
 
     constructor(id, nombre) {
-        this.id = id;
-        this.nombre = nombre;
+        this._id = id;
+        this._nombre = nombre;
     }
 
+    // GETTERS
+    get id() {
+        return this._id;
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+
+    // SETTERS
+    set id(valor) {
+        this._id = valor;
+    }
+
+    set nombre(valor) {
+        this._nombre = valor;
+    }
+
+    // MÉTODOS API
     async obtenerGeneros() {
         return await obtenerGeneros();
+    }
+
+    async agregarGenero(nombre) {
+        return await agregarGenero(nombre);
+    }
+
+    async eliminarGenero(id) {
+        return await eliminarGenero(id);
+    }
+
+    async obtenerGenero(id) {
+        return await obtenerGenero(id);
     }
 }
 
