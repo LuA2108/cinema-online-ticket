bb7d5d79a29b91e5d8ab22de64b9042320ac08f0
Lucero <luceroan2108@gmail.com>
2026-05-29 17:52:26 +0200

    Eliminación del archivo género.

--- public/vite-project/src/admin/genero.js
+++
@@ -1,6 +0,0 @@
-import GeneroControlador from "../controlador/generoControlador";
-
-document.addEventListener('DOMContentLoaded', async () => {
-    const controlador = new GeneroControlador();
-    await controlador.cargarGeneros();
-});
