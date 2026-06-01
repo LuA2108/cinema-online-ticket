053be29c3ea815368ebda0557a689bd0e908a0bd
Lucero <luceroan2108@gmail.com>
2026-06-01 10:01:33 +0200

    Modelo usuario: - Se añaden las funciones de la api faltantes. - Se elimina contraseña del constructor.

--- public/vite-project/src/models/modeloUsuario.js
+++ public/vite-project/src/models/modeloUsuario.js
@@ -1,24 +1,37 @@
-import { obtenerUsuarios } from "../api/usuariosApi";
+import {obtenerUsuarios, obtenerUsuarioPorId, crearUsuario, actualizarUsuario, eliminarUsuario} from "../api/usuariosApi";
 
 class ModeloUsuario {
-    #contrasena;
 
-    constructor(id, rol_id, nombre, email, contrasena, ciudad, provincia, create_time) {
-
+    constructor(id, rol_id , nombre, email, ciudad, provincia, create_time) 
+    {
         this.id = id;
         this.rol_id = rol_id;
         this.nombre = nombre;
         this.email = email;
-        this.contrasena = contrasena;
         this.ciudad = ciudad;
         this.provincia = provincia;
         this.create_time = create_time;
     }
 
-    async obtenerUsuarios($datos) {
+    async obtenerUsuarios() {
         return await obtenerUsuarios();
     }
 
+    async obtenerUsuarioPorId(id) {
+        return await obtenerUsuarioPorId(id);
+    }
+
+    async crearUsuario(datos) {
+        return await crearUsuario(datos);
+    }
+
+    async actualizarUsuario(id, datos) {
+        return await actualizarUsuario(id, datos);
+    }
+
+    async eliminarUsuario(id) {
+        return await eliminarUsuario(id);
+    }
 }
 
 export default ModeloUsuario;
