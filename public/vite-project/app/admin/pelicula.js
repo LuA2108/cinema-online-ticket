869bd5a331ceb68e47362d07b96615f1b88d8ba8
Lucero <luceroan2108@gmail.com>
2026-06-01 10:06:11 +0200

    Eliminacion de pelicula.js reemplazada anteriormente por peliculas.js

--- public/vite-project/app/admin/pelicula.js
+++
@@ -1,8 +0,0 @@
-import PeliculaController from "cinema-online-ticket/public/vite-proyect/src/controller/pelicula";
-import PeliculaTabla from "cinema-online-ticket/public/vite-proyect/src/view/peliculas/PeliculaTabla.js";
-
-const controller = new PeliculaController(
-    new PeliculaTabla("tablaPeliculas")
-);
-
-controller.init();
