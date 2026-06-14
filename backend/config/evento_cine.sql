fe059f6bd74a70407dfd4ace6decdf0f4628e7e1
Lucero <luceroan2108@gmail.com>
2026-06-14 20:12:19 +0200

    Modifcar el evento para actualizar el estado de programaciones y corregir el tiempo de actualizacion por minuto.

--- backend/config/evento_cine.sql
+++ backend/config/evento_cine.sql
@@ -1,9 +1,17 @@
 SET GLOBAL event_scheduler = ON;
 
 CREATE EVENT finalizar_funciones
-ON SCHEDULE EVERY 1 DAY
+ON SCHEDULE EVERY 1 MINUTE
 DO
 UPDATE funcion
-SET estado_id = 4
-WHERE estado_id IN (2)
-AND fecha_hora < NOW();
+SET estado_id = 3
+WHERE estado_id = 2
+AND fecha_hora <= NOW();
+
+CREATE EVENT finalizar_programaciones
+ON SCHEDULE EVERY 1 MINUTE
+DO
+UPDATE programacion
+SET estado = 0
+WHERE estado = 1
+AND TIMESTAMP(fecha_fin, hora) <= NOW();
