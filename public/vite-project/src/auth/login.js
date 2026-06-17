aa7de036ef0dcf5105ae3192932ea41bd75d070e
Lucero <luceroan2108@gmail.com>
2026-06-17 08:07:30 +0200

    - Decodificar JWT de forma segura y guardar datos de usuario en localStorage - Añadir comentarios

--- public/vite-project/src/auth/login.js
+++ public/vite-project/src/auth/login.js
@@ -16,11 +16,20 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
         alert("Login incorrecto");
         return;
     }
-    
     console.log(data.token);
+
     localStorage.setItem("token", data.token);
 
-    const payload = JSON.parse(atob(data.token.split(".")[1]));
+    const payload = parseJwt(data.token);
+    console.log(payload);
+
+    localStorage.setItem("usuario", JSON.stringify({
+        id: payload.id,
+        nombre: payload.nombre,
+        email: payload.email,
+        rol: payload.rol
+    })
+    );
 
     if (payload.rol === 1) {
         location.href = "../../admin.html";
@@ -28,3 +37,14 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
         location.href = "./perfil.html";
     }
 });
+
+function parseJwt(token) {
+    return JSON.parse(
+        decodeURIComponent(
+            atob(token.split('.')[1])
+                .split('')
+                .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+                .join('')
+        )
+    );
+}
