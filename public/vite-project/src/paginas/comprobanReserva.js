7ff392e4a739755c58f5c35cdac71ced6f9705d1
Lucero <luceroan2108@gmail.com>
2026-06-17 08:45:30 +0200

    Inicializador del comprobante de reservas que se enlaza con el html para mostrar los datos dinamicamente

---
+++ public/vite-project/src/paginas/comprobanReserva.js
@@ -0,0 +1,7 @@
+import ReciboControlador from "../controlador/reciboControlador";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const controlador = new ReciboControlador();
+    controlador.init(); 
+});
