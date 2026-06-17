688c7053f8f578b88dc2f068128d424bb1f4a8a5
Lucero <luceroan2108@gmail.com>
2026-06-17 08:30:59 +0200

    Inicializador de confirmacion de reserva

---
+++ public/vite-project/src/paginas/confirmacionReserva.js
@@ -0,0 +1,7 @@
+import ConfirmacionReservaControlador from "../controlador/confirmacionReservaControlador";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const controlador = new ConfirmacionReservaControlador();
+    controlador.init(); 
+});
